4d3a37eb0694d066c07ce3f537da85a441761ca0
Paul Killman <39015713+pkillman2000@users.noreply.github.com>
2023-06-21 12:03:51 -0500

    Player character added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,30 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    private CharacterController _controller;
     [SerializeField]
-    private float _speed;
+    private float _speed = 5.0f;
     [SerializeField]
-    private float _gravity;
+    private float _gravity = 1.0f;
+    [SerializeField]
+    private float _jumpHeight = 15.0f;
+    private float _yVelocity;
+    private bool _canDoubleJump = false;
+    private bool _canWallJump = false;
+    Vector3 _wallSurfaceNormal;
+    [SerializeField]
+    private float _pushVelocity;
+
     private Vector3 _direction;
     private Vector3 _velocity;
-    private float _yVelocity;
 
-    CharacterController _controller;
 
-    private void Start()
+    void Start()
     {
         _controller = GetComponent<CharacterController>();
-        if (_controller == null)
+        if(_controller == null)
         {
-            Debug.LogError("Character Controller is Null!");
+            Debug.LogError("Character Controller is null!");
         }
     }
 
-
-    private void Update()
+    void Update()
     {
         CalculateMovement();
     }
@@ -33,18 +41,66 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        if (_controller.isGrounded == true) // Character on ground - Get movement input
+        if (_controller.isGrounded == true)
         {
+            _canWallJump = true;
             _direction = new Vector3(0, 0, horizontalInput);
             _velocity = _direction * _speed;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
         }
-        else // Add gravity
+        else
         {
+            if (Input.GetKeyDown(KeyCode.Space) && _canWallJump == false) // Jump
+            {
+                if (_canDoubleJump == true) // Double Jump
+                {
+                    _yVelocity += _jumpHeight;
+                    _canDoubleJump = false;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && _canWallJump == true) // Wall Jump
+            {
+                _yVelocity += _jumpHeight;
+                _velocity = _wallSurfaceNormal * _speed;
+                _canWallJump = false;
+            }
+
             _yVelocity -= _gravity;
         }
 
-        _velocity.y = _yVelocity; // Add gravity if necessary
+        _velocity.y = _yVelocity;
 
-        _controller.Move(_velocity * Time.deltaTime); // Move character
+        if (_controller.enabled == true)
+        {
+            _controller.Move(_velocity * Time.deltaTime);
+        }
+
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (!_controller.isGrounded && hit.gameObject.tag == "Wall") // Wall Jumping
+        {
+            _canWallJump = true;
+            _wallSurfaceNormal = hit.normal;
+        }
+
+        if (hit.gameObject.tag == "Movable") // Pushing Movables
+        {
+            Rigidbody rigidbody = hit.collider.attachedRigidbody;
+            if (rigidbody == null || rigidbody.isKinematic)
+            {
+                Debug.LogWarning("Moveable Rigidbody Problems!");
+                return;
+            }
+
+            Vector3 pushDir = new Vector3(0, 0, hit.moveDirection.z);
+            rigidbody.velocity = pushDir * _pushVelocity;
+        }
     }
 }
